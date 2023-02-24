e0343fd80408d91e77116cf47157f00bbe1e59f9
Moamin Albayed <moamin_albayed@hotmail.com>
2023-02-24 15:59:49 +0300

    added placeholder and crossfade to location thumbnail

--- app/src/main/java/albayed/moamin/notesattach/components/LocationCard.kt
+++ app/src/main/java/albayed/moamin/notesattach/components/LocationCard.kt
@@ -22,11 +22,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 
 @Composable
@@ -81,8 +84,14 @@ fun LocationCard(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 AsyncImage(
-                    modifier = Modifier.aspectRatio(1f).padding(5.dp),
-                    model = fetchThumbnailUri(location),
+                    modifier = Modifier
+                        .aspectRatio(1f)
+                        .padding(5.dp),
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(fetchThumbnailUri(location))
+                        .crossfade(true)
+                        .build(),
+                    placeholder = painterResource(id = R.drawable.location),
                     contentScale = ContentScale.Crop,
                     contentDescription = "Map Thumbnail"
                 )
