715003ad21ead25bfc9d1d25507558dd5b95847e
Moamin Albayed <moamin_albayed@hotmail.com>
2023-03-15 15:56:36 +0300

    added some comments

--- app/src/main/java/albayed/moamin/composenotesattach/NotesAttachApplication.kt
+++ app/src/main/java/albayed/moamin/composenotesattach/NotesAttachApplication.kt
@@ -3,6 +3,6 @@ package albayed.moamin.composenotesattach
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
+@HiltAndroidApp //also added android:name=".NotesAttachApplication" in manifest
 class NotesAttachApplication: Application (){
 }
